86123c54f55d248bb4dc0680890a55afb358274a
Wojciech Babiński <47296815+MrFuru18@users.noreply.github.com>
2023-02-01 04:56:58 +0100

    Update OCR.bat

--- OCR.bat
+++ OCR.bat
@@ -7,14 +7,16 @@ echo Optical Character Recognition For Digits
 echo.
 echo ====  1. KNN    ====
 echo ====  2. Model  ====
-echo ====  3. Info   ====
-echo ====  4. Exit   ====
+echo ====  3. DNN    ====
+echo ====  4. Info   ====
+echo ====  5. Exit   ====
 echo.
 set /p select=">>"
 if %select%==1 goto KNN
 if %select%==2 goto Model
-if %select%==3 goto info
-if %select%==4 goto exit
+if %select%==3 goto DNN
+if %select%==4 goto info
+if %select%==5 goto exit
 
 :KNN
 cls
@@ -28,15 +30,21 @@ start weights_model.py
 goto menu
 pause
 
+:DNN
+cls
+start neural_network.py
+goto menu
+pause
+
 :info
 cls
-echo Systemy Sztucznej Inteligencji - Projekt Zaliczeniowy
+echo Zastosowania Sztucznej Inteligencji - Projekt Zaliczeniowy
 echo Optical Character Recognition For Digits
 echo.
 echo -----------------------------------------------------
 echo.
 echo Wojciech Babinski
-echo Informatyka sem IV
+echo Informatyka sem VII
 pause
 goto menu
 
